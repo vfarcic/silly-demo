414c6787085cb7015a2c3d4a000f90946e74f5af
Viktor Farcic <viktor@farcic.com>
2022-07-03 23:41:57 +0200

    Fibonacci

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.GET("/fibonacci", fibonacciHandler)
 	r.GET("/", rootHandler)
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
@@ -19,13 +21,12 @@ func main() {
 }
 
 func rootHandler(c *gin.Context) {
-	code := http.StatusOK
-	output := "This is a silly demo"
 	if len(c.Query("fail")) > 0 {
-		code = http.StatusInternalServerError
-		output = "Something terrible happened"
+		c.String(http.StatusBadRequest, "Something terrible happened")
+		return
 	}
 	version := os.Getenv("VERSION")
+	output := "This is a silly demo"
 	if len(version) > 0 {
 		output = fmt.Sprintf("%s version %s", output, version)
 	}
@@ -33,8 +34,24 @@ func rootHandler(c *gin.Context) {
 		output = fmt.Sprintf("<h1>%s</h1>", output)
 	}
 	output = fmt.Sprintf("%s\n", output)
-	c.String(code, output)
+	c.String(http.StatusOK, output)
 }
 
+// Handle the /fibonacci path
+func fibonacciHandler(c *gin.Context) {
+	// Get number parameter from query
+	number, err := strconv.Atoi(c.Query("number"))
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+	fib := calculateFibonacci(number)
+	c.String(http.StatusOK, fmt.Sprintf("%d", fib))
+}
 
-
+func calculateFibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return calculateFibonacci(n-1) + calculateFibonacci(n-2)
+}
