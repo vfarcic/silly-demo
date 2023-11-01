b2a22df916bbb91c3ac032fa09e5f35e745a5046
Viktor Farcic <viktor@farcic.com>
2023-11-01 23:15:14 +0100

    Memory leaks

--- memory.go
+++ memory.go
@@ -12,11 +12,14 @@ import (
 )
 
 func memoryLeakHandler(ctx *gin.Context) {
-	frequencyString := ctx.Query("frequency")
-	if len(frequencyString) == 0 {
-		frequencyString = "60"
+	frequency := 60 // 60 seconds
+	if len(ctx.Query("frequency")) > 0 {
+		frequency, _ = strconv.Atoi(ctx.Query("frequency"))
 	}
-	frequency, _ := strconv.Atoi(frequencyString)
+	maxMemory := 1024 * 1 // 1 GB
+	if len(ctx.Query("max-memory")) > 0 {
+		maxMemory, _ = strconv.Atoi(ctx.Query("max-memory"))
+	}
 	slice := make([]byte, 1024*1024)
 	go func() {
 		for {
@@ -24,6 +27,9 @@ func memoryLeakHandler(ctx *gin.Context) {
 			memStats := runtime.MemStats{}
 			runtime.ReadMemStats(&memStats)
 			fmt.Printf("Memory usage: %d MB\n", memStats.Alloc/1024/1024)
+			if memStats.Alloc/1024/1024 > uint64(maxMemory) {
+				break
+			}
 			time.Sleep(time.Second * time.Duration(frequency))
 		}
 	}()
