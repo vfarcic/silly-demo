fe93ee854f0e530220138dae032789dc3a0010cd
Viktor Farcic <viktor@farcic.com>
2023-11-05 15:01:45 -0600

    Memory leak

--- main.go
+++ main.go
@@ -17,6 +17,9 @@ func main() {
 	if os.Getenv("SERVICE_NAME") != "" {
 		serviceName = os.Getenv("SERVICE_NAME")
 	}
+	if os.Getenv("MEMORY_LEAK_MAX_MEMORY") != "" {
+		memoryLeak(0, 0)
+	}
 
 	// Server
 	log.Println("Starting server...")
--- memory.go
+++ memory.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"runtime"
 	"strconv"
 	"time"
@@ -12,13 +13,29 @@ import (
 )
 
 func memoryLeakHandler(ctx *gin.Context) {
+	maxMemory := 1024 * 1 // 1 GB
+	if len(ctx.Query("max-memory")) > 0 {
+		maxMemory, _ = strconv.Atoi(ctx.Query("max-memory"))
+	}
 	frequency := 60 // 60 seconds
 	if len(ctx.Query("frequency")) > 0 {
 		frequency, _ = strconv.Atoi(ctx.Query("frequency"))
 	}
-	maxMemory := 1024 * 1 // 1 GB
-	if len(ctx.Query("max-memory")) > 0 {
-		maxMemory, _ = strconv.Atoi(ctx.Query("max-memory"))
+	memoryLeak(maxMemory, frequency)
+	output := "Memory leak simulation started"
+	log.Println(output)
+	ctx.String(http.StatusOK, output)
+}
+
+func memoryLeak(maxMemory, frequency int) {
+	if maxMemory <= 0 {
+		maxMemory, _ = strconv.Atoi(os.Getenv("MEMORY_LEAK_MAX_MEMORY"))
+	}
+	if frequency <= 0 {
+		frequency = 60
+		if len(os.Getenv("MEMORY_LEAK_FREQUENCY")) > 0 {
+			frequency, _ = strconv.Atoi(os.Getenv("MEMORY_LEAK_MAX_MEMORY"))
+		}
 	}
 	slice := make([]byte, 1024*1024)
 	go func() {
@@ -33,7 +50,4 @@ func memoryLeakHandler(ctx *gin.Context) {
 			time.Sleep(time.Second * time.Duration(frequency))
 		}
 	}()
-	output := "Memory leak simulation started"
-	log.Println(output)
-	ctx.String(http.StatusOK, output)
 }
